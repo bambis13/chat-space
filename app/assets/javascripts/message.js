d819de41fd53d65d6ca9a81a4bd09e2186dbe95d
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-08 16:59:20 +0900

    非同期のコメント投稿＆保存成功！

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,66 @@
 $(function() {
 
-  $('.form').click('submit', function(e) {
+  function buildHTML(message){
+
+    var html = `<div class='message'>
+                  <div class='upper-message'>
+                    <div class='message__name'>
+                    ${message.name}
+                    </div>
+                    <div class='message__date'>
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class='lower-message'>
+                    <div class='message__text'>
+                    ${message.text}
+                    </div>
+
+                  </div>
+                </div>`
+
+//     if(message.text !== nil &message.image present?){html.append('<div class='lower-message'>
+// <div class='message__text'>
+
+// </div>
+
+// </div>')
+//     }elseif(message.text.present?){
+
+//     }elseif(message.image.present?){
+
+//     }else{
+
+//     }
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    textField = $('.form__text-field');
-    text = textField.val();
-    console.log(text);
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    // console.log(this)
+    // console.log(url)
+    // textField = $('.form__text-field');
+    // text = textField.val();
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      // console.log(html)
+      $('.contents-message').append(html)
+      // $('.text_field').val('')
+    })
+    // .fail(function(){
+    //   alert('error!');
+    // })
   });
 });
+
+
