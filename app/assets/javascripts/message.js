c029f902fcaf85ec408f7ab4a9cd658225e24add
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-09 14:52:38 +0900

    非同期通信によるメッセージと画像投稿の実装 テキストと画像両方、テキストのみ、画像のみで条件分岐

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,23 +1,42 @@
-$(function() {
+$(function(){
 
   function buildHTML(message){
 
-    var html = `<div class='message' id='latest-message'>
+    var html =
+               `<div class='message' id='latest-message'>
                   <div class='upper-message'>
                     <div class='message__name'>
-                    ${message.name}
+                      ${message.name}
                     </div>
                     <div class='message__date'>
-                    ${message.created_at}
+                      ${message.created_at}
                     </div>
-                  </div>
-                  <div class='lower-message'>
-                    <div class='message__text'>
-                    ${message.text}
-                    </div>
-                    <img class="message__image" src=${message.image} alt="">
-                  </div>
-                </div>`
+                  </div>`
+
+    var html_text =
+                   `<div class='lower-message'>
+                      <div class='message__text'>
+                        ${message.text}
+                      </div>
+                     </div>`
+
+    var html_image =
+                     `<div class='lower-message'>
+                        <div class='message__image'></div>
+                          <img src="${message.image.url}" alt=" ">
+                        </div>
+                      </div>`
+    if(message.text && message.image.url){
+      html = $(html).append(html_text + html_image + `</div>`)
+    } else if(message.image.url == null && message.text[0]){
+      html = $(html).append(html_text + `</div>`)
+    } else if(!message.text[0] && message.image.url){
+      html = $(html).append(html_image + `</div>`)
+    } else {
+      html = $(html).append(
+                            `<p>エラー！</p>
+                          </div>`)
+    }
     return html;
   }
 
@@ -25,8 +44,6 @@ $(function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    // console.log(this)
-    // console.log(url)
     $.ajax({
       url: url,
       type: 'POST',
@@ -38,9 +55,15 @@ $(function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.contents-message').append(html);
+      $('.form__text-field').val("");
+      $('.upload_image').val("");
+      $('.form__submit').prop("disabled", false);
       $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
+      $('.form__text-field').val("");
+      $('.upload_image').val("");
+      $('.form__submit').prop("disabled", false);
       alert('error!');
     })
   });
