a4651fe3cd1a38bd22de8282217b944ea196fc7d
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-16 21:55:51 +0900

    fix message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,85 +1,85 @@
-$(function(){
+// $(function(){
 
-  var reloadTimer = setInterval(function(){
-    if(location.pathname.match(/messages/)){
-      var lastMessageId = ($('.message').data()) ? lastMessageId = $('.message:last').data('id') 
-      : lastMessageId = 0
-      $.ajax({
-        url: location.href,
-        type: 'GET',
-        data: {id: lastMessageId},
-        dataType: 'json',
-      })
-      .done(function(data){
-        if (data.length > 0){
-          data.forEach(function(newMessage){
-            html = buildHTML(newMessage)
-            $('.contents-message').append(html);
-            $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight}, 'fast');
-          })
-        }
-      })
-      .fail(function(){
-         alert('メッセージの取得に失敗しています!')
-      })
-    } else {
-      clearInterval(reloadTimer);
-    }
- },5000);
+//   var reloadTimer = setInterval(function(){
+//     if(location.pathname.match(/messages/)){
+//       var lastMessageId = ($('.message').data()) ? lastMessageId = $('.message:last').data('id') 
+//       : lastMessageId = 0
+//       $.ajax({
+//         url: location.href,
+//         type: 'GET',
+//         data: {id: lastMessageId},
+//         dataType: 'json',
+//       })
+//       .done(function(data){
+//         if (data.length > 0){
+//           data.forEach(function(newMessage){
+//             html = buildHTML(newMessage)
+//             $('.contents-message').append(html);
+//             $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight}, 'fast');
+//           })
+//         }
+//       })
+//       .fail(function(){
+//          alert('メッセージの取得に失敗しています!')
+//       })
+//     } else {
+//       clearInterval(reloadTimer);
+//     }
+//  },5000);
 
-  function buildHTML(message){
+//   function buildHTML(message){
 
-    var html_head =
-               `<div class='message' id='latest-message' data-id="${message.id}">
-                  <div class='upper-message'>
-                    <div class='message__name'>
-                      ${message.name}
-                    </div>
-                    <div class='message__date'>
-                      ${message.date}
-                    </div>
-                  </div>
-                  <div class='lower-message'>
-                    <div class='message__text'>
-                      ${message.text}
-                    </div>`
-    var html_image =
-                     `<div class='message__image'>
-                        <img src="${message.image.url}" alt=" ">
-                      </div>`
+//     var html_head =
+//                `<div class='message' id='latest-message' data-id="${message.id}">
+//                   <div class='upper-message'>
+//                     <div class='message__name'>
+//                       ${message.name}
+//                     </div>
+//                     <div class='message__date'>
+//                       ${message.date}
+//                     </div>
+//                   </div>
+//                   <div class='lower-message'>
+//                     <div class='message__text'>
+//                       ${message.text}
+//                     </div>`
+//     var html_image =
+//                      `<div class='message__image'>
+//                         <img src="${message.image.url}" alt=" ">
+//                       </div>`
 
-    html = (message.image.url) ? html = $(html_head).append(html_image + `</div></div>`) : html = $(html_head).append(`</div></div>`)
+//     html = (message.image.url) ? html = $(html_head).append(html_image + `</div></div>`) : html = $(html_head).append(`</div></div>`)
 
-    return html;
-  }
+//     return html;
+//   }
 
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      console.log(data.image)
-      var html = buildHTML(data);
-      $('.contents-message').append(html);
-      $('form')[0].reset();
-      $('.form__submit').prop("disabled", false);
-      $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight}, 'fast');
-    })
-    .fail(function(){
-      $('.form__text-field').val("");
-      $('.upload_image').val("");
-      $('.form__submit').prop("disabled", false);
-      alert('error!');
-    })
-  });
-});
+//   $('#new_message').on('submit', function(e) {
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action');
+//     $.ajax({
+//       url: url,
+//       type: 'POST',
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       console.log(data.image)
+//       var html = buildHTML(data);
+//       $('.contents-message').append(html);
+//       $('form')[0].reset();
+//       $('.form__submit').prop("disabled", false);
+//       $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight}, 'fast');
+//     })
+//     .fail(function(){
+//       $('.form__text-field').val("");
+//       $('.upload_image').val("");
+//       $('.form__submit').prop("disabled", false);
+//       alert('error!');
+//     })
+//   });
+// });
 
 
