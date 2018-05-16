2bd5ca4e6945c9dc109418e04432d71a8d71aefd
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-16 17:05:57 +0900

    change migrate

--- db/migrate/20180504094308_create_messages.rb
+++ db/migrate/20180504094308_create_messages.rb
@@ -4,8 +4,8 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.text :text
       t.text :image
       t.timestamps
-      t.references :user, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
+      t.references :group,  null: false, foreign_key: true
+      t.references :user,   null: false, foreign_key: true
     end
   end
 end
